9e550bf6285453c93e8007344e82ae8afcbac0d9
Fouad Belhia <fouad51100@gmail.com>
2025-02-27 22:48:44 +0100

    Création d'un UIManager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+    [SerializeField] private Text totalMoneyLabel;
+    [SerializeField] private Image gameStatusImage;
+    [SerializeField] private Text nextWaveBtnLabel;
+    [SerializeField] private Text escapedLabel;
+    [SerializeField] private Text waveLabel;
+    [SerializeField] private Text gameStatusLabel;
+
+    protected override void Awake()
+    {
+        base.Awake(); // Initialisation du Singleton
+    }
+
+    public void HideGameStatus()
+    {
+        gameStatusImage.gameObject.SetActive(false);
+    }
+
+
+    public void UpdateMoneyDisplay(int totalMoney)
+    {
+        totalMoneyLabel.text = totalMoney.ToString();
+    }
+
+    public void UpdateEscapedDisplay(int totalEscaped)
+    {
+        escapedLabel.text = "Escaped " + totalEscaped + "/10";
+    }
+
+    public void UpdateWaveDisplay(int waveNumber)
+    {
+        waveLabel.text = "Wave " + (waveNumber + 1);
+    }
+
+    public void ShowGameStatus(gameStatus status, AudioSource audioSource)
+    {
+        switch (status)
+        {
+            case gameStatus.gameover:
+                gameStatusLabel.text = "Gameover";
+                audioSource.PlayOneShot(SoundManager.Instance.Gameover);
+                nextWaveBtnLabel.text = "Play again";
+                break;
+            case gameStatus.next:
+                nextWaveBtnLabel.text = "Next Wave";
+                gameStatusLabel.text = "Wave " + (GameManager.Instance.WaveNumber + 2) + " next.";
+                break;
+            case gameStatus.play:
+                nextWaveBtnLabel.text = "Play";
+                break;
+            case gameStatus.win:
+                nextWaveBtnLabel.text = "Play";
+                gameStatusLabel.text = "You Won!";
+                break;
+        }
+        gameStatusImage.gameObject.SetActive(true);
+    }
+}
