b558a3fd7fa423e9032a1abba34d44171dafa16a
Fouad Belhia <fouad51100@gmail.com>
2025-02-27 21:58:15 +0100

    Modification GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public enum gameStatus
@@ -13,8 +13,7 @@ public class GameManager : Singleton<GameManager>
     const float waitingTime = 1f;
 
     [SerializeField] private int totalWaves = 10;
-    [SerializeField] private List<GameObject> spawnPoints;
-    [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private List<Transform> spawnPoints;
     [SerializeField] private int totalEnemies = 3;
     [SerializeField] private int enemiesPerSpawn;
     [SerializeField] private Text totalMoneyLabel;
@@ -24,10 +23,11 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private Text waveLabel;
     [SerializeField] private Text GameStatusLabel;
     [SerializeField] private int waveNumber = 0;
+    [SerializeField] private EnemySpawner enemySpawner;
 
     private int totalMoney = 10;
-    private int totalEscaped = 0;      // Escapes cumulées
-    private int roundEscaped = 0;        // Escapes pour la vague en cours
+    private int totalEscaped = 0;
+    private int roundEscaped = 0;
     private int totalKilled = 0;
     private int enemiesToSpawn = 0;
     private gameStatus currentState = gameStatus.play;
@@ -42,23 +42,12 @@ public class GameManager : Singleton<GameManager>
         set
         {
             totalEscaped = value;
-            if (escapedLabel != null)
-                escapedLabel.text = "Escaped " + totalEscaped + "/10";
+            escapedLabel.text = "Escaped " + totalEscaped + "/10";
         }
     }
 
-    // Permet aux scripts ennemis d'incrémenter le compteur
-    public int RoundEscaped
-    {
-        get => roundEscaped;
-        set => roundEscaped = value;
-    }
-
-    public int TotalKilled
-    {
-        get => totalKilled;
-        set { totalKilled = value; }
-    }
+    public int RoundEscaped { get => roundEscaped; set => roundEscaped = value; }
+    public int TotalKilled { get => totalKilled; set => totalKilled = value; }
 
     public int TotalMoney
     {
@@ -69,6 +58,7 @@ public class GameManager : Singleton<GameManager>
             totalMoneyLabel.text = totalMoney.ToString();
         }
     }
+
     public AudioSource AudioSource => audioSource;
 
     void Start()
@@ -87,36 +77,20 @@ public class GameManager : Singleton<GameManager>
         }
     }
 
-    // Ces méthodes ne font plus gérer la liste d'ennemis localement,
-    // elles délèguent directement à EnemyManager.
     public void RegisterEnemy(Enemy enemy)
     {
-        if (enemy != null)
-        {
-            EnemyManager.Instance.RegisterEnemy(enemy);
-        }
+        EnemyManager.Instance.RegisterEnemy(enemy);
     }
 
     public void UnRegister(Enemy enemy)
     {
-        if (enemy != null)
-        {
-            EnemyManager.Instance.UnregisterEnemy(enemy);
-        }
+        EnemyManager.Instance.UnregisterEnemy(enemy);
         isWaveOver();
     }
 
-    public void addMoney(int amount)
-    {
-        TotalMoney += amount;
-    }
+    public void addMoney(int amount) => TotalMoney += amount;
+    public void subtractMoney(int amount) => TotalMoney -= amount;
 
-    public void subtractMoney(int amount)
-    {
-        TotalMoney -= amount;
-    }
-
-    // Vérifie si la vague est terminée (c'est-à-dire si le nombre d'ennemis tués et d'évasions atteint totalEnemies).
     public void isWaveOver()
     {
         if ((roundEscaped + totalKilled) >= totalEnemies)
@@ -130,21 +104,13 @@ public class GameManager : Singleton<GameManager>
     public void setCurrentGameState()
     {
         if (TotalEscaped >= 10)
-        {
             currentState = gameStatus.gameover;
-        }
         else if (waveNumber == 0 && (totalKilled + roundEscaped) == 0)
-        {
             currentState = gameStatus.play;
-        }
         else if (waveNumber >= totalWaves)
-        {
             currentState = gameStatus.win;
-        }
         else
-        {
             currentState = gameStatus.next;
-        }
     }
 
     public void nextWavePressed()
@@ -169,50 +135,14 @@ public class GameManager : Singleton<GameManager>
                 break;
         }
 
-        // On vérifie maintenant la liste d'ennemis via EnemyManager
         if (EnemyManager.Instance.EnemyList.Count > 0)
             DestroyAllEnemies();
 
         totalKilled = 0;
-        roundEscaped = 0; // Réinitialisation pour la nouvelle vague.
+        roundEscaped = 0;
         waveLabel.text = "Wave " + (waveNumber + 1);
-        StartCoroutine(SpawnEnemies());
+        StartCoroutine(enemySpawner.SpawnEnemies(totalEnemies, enemiesPerSpawn));
         GameStatusImage.gameObject.SetActive(false);
-    }
-
-    IEnumerator SpawnEnemies()
-    {
-        if (enemyPrefabs == null || enemyPrefabs.Length == 0)
-        {
-            Debug.LogWarning("Aucun prefab d'ennemi n'est assigné. Assignez-le dans l'Inspector.");
-            yield break;
-        }
-
-        // On utilise désormais EnemyManager.Instance.EnemyList.Count pour comparer
-        if (enemiesPerSpawn > 0 && EnemyManager.Instance.EnemyList.Count < totalEnemies)
-        {
-            for (int i = 0; i < enemiesPerSpawn; i++)
-            {
-                if (EnemyManager.Instance.EnemyList.Count < totalEnemies)
-                {
-                    GameObject enemyGO = Instantiate(enemyPrefabs[0]);
-                    GameObject spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
-                    enemyGO.transform.position = spawnPoint.transform.position;
-                    Enemy enemyComp = enemyGO.GetComponent<Enemy>();
-                    if (enemyComp != null)
-                    {
-                        // L'enregistrement de l'ennemi est normalement effectué dans Enemy.Start().
-                        // Si nécessaire, vous pouvez aussi appeler EnemyManager.Instance.RegisterEnemy(enemyComp);
-                    }
-                    else
-                    {
-                        Debug.LogError("L'ennemi instancié ne possède pas le composant Enemy !");
-                    }
-                }
-            }
-            yield return new WaitForSeconds(waitingTime);
-            StartCoroutine(SpawnEnemies());
-        }
     }
 
     public void DestroyAllEnemies()
