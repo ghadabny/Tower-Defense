cb6f9d7c855b500b6fd281ae6a6ab63121e6741a
Fouad Belhia <fouad51100@gmail.com>
2025-03-07 21:58:37 +0100

    Ajout d'un scoreManager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;      
+    [SerializeField] private Text bestScoreText;
+
+    private int currentWave = 0;
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+
+    public void WaveCompleted()
+    {
+        currentWave++;
+        UpdateUI();
+        SaveBestScore();
+    }
+
+    private void UpdateUI()
+    {
+        scoreText.text = "Vagues : " + currentWave;
+
+        int best = PlayerPrefs.GetInt("BestScore", 0);
+        bestScoreText.text = "Meilleur score : " + best;
+    }
+
+    private void SaveBestScore()
+    {
+        int best = PlayerPrefs.GetInt("BestScore", 0);
+
+        if (currentWave > best)
+        {
+            PlayerPrefs.SetInt("BestScore", currentWave);
+            PlayerPrefs.Save();
+        }
+
+        UpdateUI();
+    }
+
+    public void ResetScore()
+    {
+        currentWave = 0;
+        UpdateUI();
+    }
+
+}
